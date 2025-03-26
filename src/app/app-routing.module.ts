f98fb21feddf9193c1bbcde75b3fcdbfb317c585
Raismoez <raismoez7@gmail.com>
2025-03-26 02:44:03 +0100

    aaa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { profilAComponent } from './profilA/profilA.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { ProfilUComponent } from './profil-u/profil-u.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { StockComponent } from './stock/stock.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent }, 
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'list', component: UserListComponent },
   { path: 'profilU', component: ProfilUComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'stock', component: StockComponent },
 
 ];
 
